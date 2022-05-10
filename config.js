f84865b8740145b2a19b073898fea544575dd56c
Yonglin Li <yonglin.li@crl.com>
2022-05-10 17:44:57 -0400

    fix: make it work

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {}
 
-config.endpoint = "wss://DATABASE_ACCOUNT_NAME.gremlin.cosmosdb.azure.com:443/gremlin";
-config.primaryKey = "PRIMARYKEY";
-config.database = "GRAPHDATABASE"
-config.collection = "GRAPHCOLLECTION"
+config.endpoint = "wss://yonglin-li-01.gremlin.cosmos.azure.com:443/";
+config.primaryKey = "Fp6uvG4Ko3lBFDpKakmuBvTQdX89OgiOS5Ro57vwb19Q2pnQtfL7aljoorLk5s64K5QyysDFyozY39TZ5IjiAQ==";
+config.database = "yonglin"
+config.collection = "yonglin"
 
 module.exports = config;
