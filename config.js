9ada2c922937bf79001b30e31efce1f890cda8c4
Yonglin Li <yonglin.li@crl.com>
2022-05-16 22:06:22 -0400

    fix: add data

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {}
 
-config.endpoint = "wss://yonglin-li-01.gremlin.cosmos.azure.com:443/";
-config.primaryKey = "Fp6uvG4Ko3lBFDpKakmuBvTQdX89OgiOS5Ro57vwb19Q2pnQtfL7aljoorLk5s64K5QyysDFyozY39TZ5IjiAQ==";
-config.database = "yonglin"
-config.collection = "yonglin"
+config.endpoint = "wss://azus-fun-cdb-01.gremlin.cosmos.azure.com:443/"
+config.primaryKey = "Jb7WJTZUuiVRcIW9L55nuvvtLzHCNBg1JbCRxxN1oCRYNSnhYv8P8Q8A0VCKiT2dwreFj6cisOM4oYAXTVy5cQ==";
+config.database = "cosmos-db"
+config.collection = "role-graph"
 
 module.exports = config;
---
+++ hotmail_yl_config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.endpoint = "wss://yonglin-li-01.gremlin.cosmos.azure.com:443/";
+config.primaryKey = "Fp6uvG4Ko3lBFDpKakmuBvTQdX89OgiOS5Ro57vwb19Q2pnQtfL7aljoorLk5s64K5QyysDFyozY39TZ5IjiAQ==";
+config.database = "yonglin"
+config.collection = "yonglin"
+
+module.exports = config;
